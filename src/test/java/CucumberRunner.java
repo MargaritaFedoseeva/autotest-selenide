2778d9f920b73535fde6b541c97d84eef933873c
mfedoseeva <fmargoha@rambler.ru>
2019-03-29 14:19:42 +0600

    git

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -1,18 +1,16 @@
-package ui.tests.runners;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features/contracts/addСontract.feature"}, glue = {"ui.steps"},
+@CucumberOptions(features = {"src/test/resources/contracts/addСontract.feature"}, glue = {"ui.steps"},
         plugin = {"pretty",
                 "json:D://reports/cucumber.json",
               "junit:D://reports/cucumber.xml"})
 public class CucumberRunner {
     public static void main(String[] args) throws Exception {
         JUnitCore.main(
-                "ui.tests.runners.CucumberRunner");
+                "CucumberRunner");
     }
 }
