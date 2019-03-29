82a8a7f6fa10a1d18edb2a8cafc6577310e36e78
mfedoseeva <fmargoha@rambler.ru>
2019-03-29 16:47:21 +0600

    git

--- src/main/java/ui/steps/BaseSteps.java
+++ src/main/java/ui/steps/BaseSteps.java
@@ -2,16 +2,9 @@ package ui.steps;
 
 import com.codeborne.selenide.Selenide;
 import cucumber.api.java.Before;
-import ui.util.TestProperties;
 
-import java.util.Properties;
 public class BaseSteps {
-    public static Properties properties = TestProperties.getInstance().getProperties();
 
-//    public static BrowserWebDriverContainer chrome =
-//            new BrowserWebDriverContainer()
-//                    .withDesiredCapabilities(DesiredCapabilities.chrome())
-//                    .withRecordingMode(RECORD_ALL, new File("./target/"));
     @Before
     public void startScenario() {
         System.setProperty("webdriver.chrome.driver", "C://Users/MFedoseeva/Desktop/chromedriver.exe");
